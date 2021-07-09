17a0009ad7678fe3b5216c9715cb593e50ff968c
Boris Bolshakov <bolsh.bo@yandex.ru>
2021-07-09 22:44:39 +0300

    Initial commit

---
+++ Swift/SwiftLesson01.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// Unit 1. A little bit about student
+
+let studentName = "Boris"
+let studentSurname = "Bolsh"
+let studentAge = 33
+
+let studentHeight = 187
+let studentWeight = 84
+
+let studentHobby = "Learning iOS development"
+
