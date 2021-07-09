b0924602aeb74baf5f12f1a70d9fbe4c7543aee8
Boris Bolshakov <bolsh.bo@yandex.ru>
2021-07-09 23:16:40 +0300

    1 lesson completed

--- Swift/SwiftLesson01.playground/Contents.swift
+++ Swift/SwiftLesson01.playground/Contents.swift
@@ -9,5 +9,20 @@ let studentAge = 33
 let studentHeight = 187
 let studentWeight = 84
 
+let studentProfession = "UiUx designer"
 let studentHobby = "Learning iOS development"
 
+
+print("""
+A little bit about student
+
+Name: \(studentName)
+Surname: \(studentSurname)
+Age: \(studentAge) yo
+
+Height: \(studentHeight) cm
+Weight: \(studentWeight) kg
+
+Profession: \(studentProfession)
+Hobby: \(studentHobby)
+""")
