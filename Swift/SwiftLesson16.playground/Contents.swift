93bbe83b0d7b865449f6e8e90290e0c4cf62bee2
Boris Bolshakov <bolsh.bo@yandex.ru>
2021-07-18 15:11:18 +0300

    Swift lesson 16 part 1 compleated

---
+++ Swift/SwiftLesson16.playground/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+// Unit 16. Methods
+// vk.com/topic-58860049_31835726
+
+struct Room {
+    var weight : Int
+    var height : Int
+}
+
+struct Player {
+ 
+    var x: Int
+    var y: Int
+    
+    mutating func walk(direct: Direction) {
+        switch direct {
+        case .Up :
+            self.y = y - 1
+        case .Down :
+            self.y = y + 1
+        case .Right :
+            self.x = x + 1
+        case .Left :
+            self.x = x - 1
+        }
+    }
+}
+
+struct Box {
+    var x: Int
+    var y: Int
+    
+    mutating func move(direct: Direction) {
+        
+        switch direct {
+        case .Up :
+            self.y = y - 1
+        case .Down :
+            self.y = y + 1
+        case .Right :
+            self.x = x + 1
+        case .Left :
+            self.x = x - 1
+        }
+    }
+    
+}
+
+struct Point {
+    var x: Int
+    var y: Int
+}
+
+enum Direction {
+    case Left, Right, Up, Down
+}
+
