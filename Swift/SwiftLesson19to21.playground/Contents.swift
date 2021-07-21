4435de9fe1b9334029833a82f00c8d26b078529e
Boris Bolshakov <bolsh.bo@yandex.ru>
2021-07-21 13:05:43 +0300

    Swift lesson 19-20 updated

--- Swift/SwiftLesson19to21.playground/Contents.swift
+++ Swift/SwiftLesson19to21.playground/Contents.swift
@@ -53,3 +53,45 @@ class Student : Human {
         test()
     }
 }
+
+
+class Doctor : Student {
+    
+    var fieldOfStudy : String
+    
+    init(fieldOfStudy: String) {
+        self.fieldOfStudy = fieldOfStudy
+        super.init(firstName: "Doctor", lastName: "House")
+    }
+    /*
+    override init(weight: Int, age: Int) {
+        self.fieldOfStudy = ""
+        super.init(weight: weight, age: age)
+    }*/
+    
+    override init(firstName: String, lastName: String) {
+        self.fieldOfStudy = "Health"
+        super.init(firstName: firstName, lastName: lastName)
+    }
+
+    /*
+    convenience init(fieldOfStudy: String) {
+        self.init(firstName: "Doctor", lastName: "House")
+        self.fieldOfStudy = fieldOfStudy
+    }
+*/
+    convenience init(firstName: String) {
+        self.init(fieldOfStudy: "Math")
+        self.age = 35
+        self.firstName = firstName
+    }
+    
+}
+
+let s1 = Student()
+
+let d1 = Doctor(firstName: "aaa")
+d1.age
+
+
+let d2 = Doctor()
